1ccba11d03fe4e8472e1b1c423e0966250c573ad
Dat Truong <mr.anhdat@gmail.com>
2014-06-03 08:59:23 +0700

    Initial Commit

---
+++ Swifting Through/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swifting Through
+//
+//  Created by Dat Truong on 2014-06-03.
+//  Copyright (c) 2014 Dat Truong. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Swifting Through/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Swifting Through
+//
+//  Created by Dat Truong on 2014-06-03.
+//  Copyright (c) 2014 Dat Truong. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
